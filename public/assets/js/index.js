33fd57e1ffa0ec1d2fffd738e939a0021dbb4102
AMJoshaghani <46087303+AMJoshaghani@users.noreply.github.com>
2021-09-26 10:11:27 +0330

    removing jQuery

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,3 +1,4 @@
+/* FUCK jQuery, we use vanilla js!
 $( document ).ready(function() {
   let bg = 'https://picsum.photos/' + screen.width + '/' + screen.height;
   $( 'body' ).css( 'background-image', `url(${bg})` );
@@ -5,3 +6,27 @@ $( document ).ready(function() {
     $( this ).fadeOut( "slow" );
   });
 });
+*/
+function $(selector){
+  return document.querySelector(selector);
+}
+
+function fadeOutEffect(selector, time) {
+    var fadeTarget = $(selector);
+    var fadeEffect = setInterval(function () {
+        if (!fadeTarget.style.opacity) {
+            fadeTarget.style.opacity = 1;
+        }
+        if (fadeTarget.style.opacity > 0) {
+            fadeTarget.style.opacity -= 0.1;
+        } else {
+            clearInterval(fadeEffect);
+        }
+    }, time);
+}
+
+window.onload = function(){
+  let bg = 'https://picsum.photos/' + screen.width + '/' + screen.height;
+  $( 'body' ).style.backgroundImage = `url(${bg})`;
+  $( '.basement' ).ondblclick = fadeOutEffect( '.basement', 1000 );
+}
