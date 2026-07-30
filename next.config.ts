aaf7ae4f98e39204a56193be2281e6266e8462cc
AMJoshaghani <amjoshaghani@gmail.com>
2026-07-31 00:55:37 +0330

    add build

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   output: 'export',
   reactStrictMode: true,
-  basePath: "",
+  basePath: "/amjoshaghani.ir",
 }
 
 export default nextConfig
