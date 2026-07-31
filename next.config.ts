954840497669778c23cf09188b65a185566c093f
AMJoshaghani <amjoshaghani@gmail.com>
2026-07-31 18:13:19 +0330

    Fix base path for amjoshaghani.ir

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   output: 'export',
   reactStrictMode: true,
-  basePath: "/amjoshaghani.ir",
+  basePath: "",
   images: {
     unoptimized: true,
   },
